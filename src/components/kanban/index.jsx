decf2290069fbebdf933cf7f683edace32106236
SeungHyun Lee <93538221+222SeungHyun@users.noreply.github.com>
2022-11-30 04:22:52 +0900

    [Feat] 칸반보드, 가이드라인, 자유공간 통합

--- src/components/kanban/index.jsx
+++ src/components/kanban/index.jsx
@@ -5,6 +5,20 @@ import { useState } from 'react'
 import Card from '../card'
 import React from 'react'
 
+import Prism from 'prismjs';
+// 여기 css를 수정해서 코드 하이라이팅 커스텀 가능
+import 'prismjs/themes/prism.css';
+
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+
+import 'tui-color-picker/dist/tui-color-picker.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+
 const Kanban = () => {
     const [data, setData] = useState(mockData)
 
@@ -33,7 +47,27 @@ const Kanban = () => {
     }
 
     return (
-        <DragDropContext onDragEnd={onDragEnd}>
+
+        <div>
+      <div>
+        <textarea placeholder="CodeConvention을 입력해주세요"></textarea>
+      </div>
+      <div>
+        <form>
+          <label>
+            19:00~20:00
+            <input type="text" />
+            <br />
+            20:00~21:00
+            <input type="text" />
+          </label>
+        </form>
+      </div>
+      <Editor
+        previewStyle="vertical"
+        plugins={[colorSyntax, [codeSyntaxHighlight, { highlighter: Prism }]]}
+      />
+      <DragDropContext onDragEnd={onDragEnd}>
             <div className="kanban">
                 {
                     data.map(section => (
@@ -85,6 +119,8 @@ const Kanban = () => {
                 }
             </div>
         </DragDropContext>
+    </div>
+        
     )
 }
 
