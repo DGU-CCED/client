0a6b6d3e8d8c08b1cb3db6428a0226476ac8aac0
SeungHyun Lee <93538221+222SeungHyun@users.noreply.github.com>
2022-12-02 03:46:34 +0900

    [Feat] 진행상황 CSS 수정 및 기능추가

--- src/components/progress/progress.jsx
+++ src/components/progress/progress.jsx
@@ -1,28 +1,62 @@
-import { Line, Circle } from 'rc-progress';
+import { Line } from 'rc-progress';
+import './progress.scss';
+import { useState, useMemo } from 'react';
+import React from 'react';
 
-export default () => (
-    <>
+export default function () {
+  const [team] = useState([
+    {
+      id: 1,
+      percent: 50,
+    },
+    {
+      id: 2,
+      percent: 60,
+    },
+    {
+      id: 3,
+      percent: 40,
+    },
+    {
+      id: 4,
+      percent: 50,
+    },
+    {
+      id: 5,
+      percent: 50,
+    },
+  ]);
+
+  return (
+    <div className="background">
+      <div className="wrapper">
         <div>
-            <div>
-                <p>해커톤 진행률</p>
+          <p className="title">해커톤 진행률</p>
+        </div>
+        <div>
+          <p className="subTitle">전체 진행도</p>
+          <div className="progressWrapper">
+            <div className="progress">
+              <Line percent="50"></Line>
             </div>
+            <p>50%</p>
+          </div>
+        </div>
+        <div>
+          <p className="subTitle">각 팀별 진행도</p>
+          {team.map((section) => (
             <div>
-                <p>전체 진행도</p>
-                <Line percent={80} strokeWidth={5} strokeColor="#FF008C" trailColor="#000000" />
+              <p className="p">{section.id}팀</p>
+              <div className="progressWrapper">
+                <div className="progress">
+                  <Line percent={section.percent}></Line>
+                </div>
+                <p>{section.percent}%</p>
+              </div>
             </div>
-            <div>
-                <p>각 팀별 진행도</p>
-                <p>1팀</p>
-                <Line percent={50} strokeWidth={5} strokeColor="#FF008C" trailColor="#000000" />
-                <p>2팀</p>
-                <Line percent={60} strokeWidth={5} strokeColor="#FF008C" trailColor="#000000" />
-                <p>3팀</p>
-                <Line percent={40} strokeWidth={5} strokeColor="#FF008C" trailColor="#000000" />
-                <p>4팀</p>
-                <Line percent={50} strokeWidth={5} strokeColor="#FF008C" trailColor="#000000" />
-                <p>5팀</p>
-                <Line percent={50} strokeWidth={5} strokeColor="#FF008C" trailColor="#000000" />
-            </div >
-        </div >
-    </>
-);
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
