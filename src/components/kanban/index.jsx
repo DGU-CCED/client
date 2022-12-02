11a26e0fb270f0adf149432ac384dee928d99e71
SeungHyun Lee <93538221+222SeungHyun@users.noreply.github.com>
2022-12-02 23:14:06 +0900

    [Add] 지원자 조회 View 구현

--- src/components/kanban/index.jsx
+++ src/components/kanban/index.jsx
@@ -142,7 +142,23 @@ const Kanban = () => {
     );
   };
 
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    // {
+    //   id: 1,
+    //   text: '리액트의 기초 알아보기',
+    //   checked: true,
+    // },
+    // {
+    //   id: 2,
+    //   text: '컴포넌트 스타일링 해보기',
+    //   checked: true,
+    // },
+    // {
+    //   id: 3,
+    //   text: '일정 관리 앱 만들어 보기',
+    //   checked: false,
+    // },
+  ]);
 
   // 고윳값으로 사용될 id
   // ref를 사용하여 변수 담기
