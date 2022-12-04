2c2939dfbf81722b675ab40a93fa90733659cf88
SeungHyun Lee <93538221+222SeungHyun@users.noreply.github.com>
2022-12-04 17:52:05 +0900

    [Feat] Create View Scrollbar 추가 및 CSS 수정

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, memo } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -6,6 +6,30 @@ import DatePicker from 'react-datepicker'; // DatePicker 라는 컴포넌트도 
 import 'react-datepicker/dist/react-datepicker.css'; // 스타일 맥이기
 import './react-datepicker.css';
 import './Create.css';
+
+const AlwaysScrollSection = memo((props) => {
+  const { children } = props;
+  return <StyledAlwaysScrollSection>{children}</StyledAlwaysScrollSection>;
+});
+
+const StyledAlwaysScrollSection = styled.div`
+  overflow: scroll;
+  height: 600px;
+  &::-webkit-scrollbar {
+    /* 세로 스크롤 넓이 */
+    width: 8px;
+
+    /* 가로 스크롤 높이 */
+    height: 0px;
+
+    border-radius: 6px;
+    background: rgba(255, 255, 255, 0.4);
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.3);
+    border-radius: 6px;
+  }
+`;
 
 const ImgPreview = styled.img`
   border: 3px solid #ffffff;
@@ -15,29 +39,26 @@ const ImgPreview = styled.img`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-left: 30%;
 `;
 
 const UploadImage = styled.input`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-left: 30%;
+  margin-left: 180px;
 `;
 
 const Create = () => {
-    const [name, setName] = useState('');
-    const [location, setLocation] = useState('');
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState('');
-    const [pm, setPm] = useState(0);
-    const [developer, setDeveloper] = useState(0);
-    const [designer, setDesigner] = useState(0);
-    const [description, setDescription] = useState('');
-    const navigate = useNavigate();
-    const [img, setImg] = useState('');
-
-  
+  const [name, setName] = useState('');
+  const [location, setLocation] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState('');
+  const [pm, setPm] = useState(0);
+  const [developer, setDeveloper] = useState(0);
+  const [designer, setDesigner] = useState(0);
+  const [description, setDescription] = useState('');
+  const navigate = useNavigate();
+  const [img, setImg] = useState('');
 
   //   const formSubmit = (e) => {
   //     const img = e.target.files[0];
@@ -58,35 +79,35 @@ const Create = () => {
     setImg(response.data);
   };
 
-    const onNameHandler = (event) => {
-        setName(event.currentTarget.value);
-    };
-    const onLocationHandler = (event) => {
-        setLocation(event.currentTarget.value);
-    };
-    const onStartDateHandler = (event) => {
-        setStartDate(event.currentTarget.value);
-    };
-    const onEndDateHandler = (event) => {
-        setEndDate(event.currentTarget.value);
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+  const onLocationHandler = (event) => {
+    setLocation(event.currentTarget.value);
+  };
+  const onStartDateHandler = (event) => {
+    setStartDate(event.currentTarget.value);
+  };
+  const onEndDateHandler = (event) => {
+    setEndDate(event.currentTarget.value);
 
-        console.log(startDate);
-    };
-    const onPmHandler = (event) => {
-        setPm(event.currentTarget.value);
-    };
-    const onDeveloperHandler = (event) => {
-        setDeveloper(event.currentTarget.value);
-    };
-    const onDesignerHandler = (event) => {
-        setDesigner(event.currentTarget.value);
-    };
-    const onDescriptionHandler = (event) => {
-        setDescription(event.currentTarget.value);
-    };
-    const onClickButton = () => {
-        navigate('/management');
-    };
+    console.log(startDate);
+  };
+  const onPmHandler = (event) => {
+    setPm(event.currentTarget.value);
+  };
+  const onDeveloperHandler = (event) => {
+    setDeveloper(event.currentTarget.value);
+  };
+  const onDesignerHandler = (event) => {
+    setDesigner(event.currentTarget.value);
+  };
+  const onDescriptionHandler = (event) => {
+    setDescription(event.currentTarget.value);
+  };
+  const onClickButton = () => {
+    navigate('/management');
+  };
 
   useEffect(() => {
     setImg();
@@ -126,139 +147,141 @@ const Create = () => {
   return (
     <>
       <div className="Wrapper">
-        <h1 style={{color: 'white', fontSize: '50px'}}>대회 개설하기</h1>
+        <h1 style={{ color: 'white', fontSize: '50px' }}>대회 개설하기</h1>
 
         <div className="FormWrap">
           <form className="form" style={{ color: 'white' }}>
-            <p className="text">해커톤 이름</p>
-            <div>
-              <input
-                className="input"
-                name="name"
-                type="text"
-                placeholder="해커톤 이름"
-                value={name}
-                onChange={onNameHandler}
-              />
-            </div>
-            <p className="text">시작 날짜</p>
-            <div>
-              <DatePicker
-                className="input"
-                minDate={new Date()}
-                closeOnScroll={true}
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-                showTimeSelect
-                timeFormat="HH:mm"
-                timeIntervals={30}
-                timeCaption="time"
-                dateFormat="yyyy MM/dd, hh:mm aa"
-                popperPlacement="auto"
-                dayClassName={(date) =>
-                  getDayName(createDate(date)) === '토'
-                    ? 'saturday'
-                    : getDayName(createDate(date)) === '일'
-                    ? 'sunday'
-                    : undefined
-                }
-              />
-            </div>
-            <p className="text">종료 날짜</p>
-            <div>
-              <DatePicker
-                className="input"
-                minDate={startDate}
-                closeOnScroll={true}
-                selected={endDate}
-                onChange={(date) => setEndDate(date)}
-                showTimeSelect
-                timeFormat="HH:mm"
-                timeIntervals={30}
-                timeCaption="time"
-                dateFormat="yyyy MM/dd, hh:mm aa"
-                popperPlacement="auto"
-                dayClassName={(date) =>
-                  getDayName(createDate(date)) === '토'
-                    ? 'saturday'
-                    : getDayName(createDate(date)) === '일'
-                    ? 'sunday'
-                    : undefined
-                }
-              />
-            </div>
-            <p className="text">진행 장소</p>
-            <div>
-              <input
-                className="input"
-                name="location"
-                type="text"
-                placeholder="진행 장소"
-                value={location}
-                onChange={onLocationHandler}
-              />
-            </div>
-            <p className="text">팀 구성</p>
-            <span className="teamBox1">
-              <span>기획자</span>
-              <span>
+            <AlwaysScrollSection>
+              <p className="text">해커톤 이름</p>
+              <div>
                 <input
-                  type="number"
-                  className="teamMember"
-                  onChange={onPmHandler}
+                  className="input"
+                  name="name"
+                  type="text"
+                  placeholder="해커톤 이름"
+                  value={name}
+                  onChange={onNameHandler}
                 />
-              </span>
-            </span>
-            <span className="teamBox2">
-              <span>개발자</span>
-              <span>
+              </div>
+              <p className="text">시작 날짜</p>
+              <div>
+                <DatePicker
+                  className="input"
+                  minDate={new Date()}
+                  closeOnScroll={true}
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                  showTimeSelect
+                  timeFormat="HH:mm"
+                  timeIntervals={30}
+                  timeCaption="time"
+                  dateFormat="yyyy MM/dd, hh:mm aa"
+                  popperPlacement="auto"
+                  dayClassName={(date) =>
+                    getDayName(createDate(date)) === '토'
+                      ? 'saturday'
+                      : getDayName(createDate(date)) === '일'
+                      ? 'sunday'
+                      : undefined
+                  }
+                />
+              </div>
+              <p className="text">종료 날짜</p>
+              <div>
+                <DatePicker
+                  className="input"
+                  minDate={startDate}
+                  closeOnScroll={true}
+                  selected={endDate}
+                  onChange={(date) => setEndDate(date)}
+                  showTimeSelect
+                  timeFormat="HH:mm"
+                  timeIntervals={30}
+                  timeCaption="time"
+                  dateFormat="yyyy MM/dd, hh:mm aa"
+                  popperPlacement="auto"
+                  dayClassName={(date) =>
+                    getDayName(createDate(date)) === '토'
+                      ? 'saturday'
+                      : getDayName(createDate(date)) === '일'
+                      ? 'sunday'
+                      : undefined
+                  }
+                />
+              </div>
+              <p className="text">진행 장소</p>
+              <div>
                 <input
-                  type="number"
-                  className="teamMember"
-                  onChange={onDeveloperHandler}
+                  className="input"
+                  name="location"
+                  type="text"
+                  placeholder="진행 장소"
+                  value={location}
+                  onChange={onLocationHandler}
                 />
+              </div>
+              <p className="text">팀 구성</p>
+              <span className="teamBox1">
+                <span>기획자</span>
+                <span>
+                  <input
+                    type="number"
+                    className="teamMember"
+                    onChange={onPmHandler}
+                  />
+                </span>
               </span>
-            </span>
-            <span className="teamBox3">
-              <span>디자이너</span>
-              <span>
-                <input
-                  type="number"
-                  className="teamMember"
-                  onChange={onDesignerHandler}
+              <span className="teamBox2">
+                <span>개발자</span>
+                <span>
+                  <input
+                    type="number"
+                    className="teamMember"
+                    onChange={onDeveloperHandler}
+                  />
+                </span>
+              </span>
+              <span className="teamBox3">
+                <span>디자이너</span>
+                <span>
+                  <input
+                    type="number"
+                    className="teamMember"
+                    onChange={onDesignerHandler}
+                  />
+                </span>
+              </span>
+              <p className="text">해커톤 이미지</p>
+              <div className="img-preview">
+                <ImgPreview src={img} alt="이미지 선택 전" />
+              </div>
+              <div>
+                <UploadImage
+                  type="file"
+                  accept="image/*"
+                  id="img"
+                  onChange={formSubmit}
+                ></UploadImage>
+              </div>
+              <p className="text">해커톤 소개</p>
+              <div>
+                <textarea
+                  rows={1}
+                  name="description"
+                  type="textarea"
+                  wrap="on"
+                  placeholder="자기소개"
+                  value={description}
+                  onChange={onDescriptionHandler}
+                  className="loginregister__desc"
                 />
-              </span>
-            </span>
-            <p className="text">해커톤 이미지</p>
-            <div className="img-preview">
-              <ImgPreview src={img} alt="이미지 선택 전" />
-            </div>
-            <div>
-              <UploadImage
-                type="file"
-                accept="image/*"
-                id="img"
-                onChange={formSubmit}
-              ></UploadImage>
-            </div>
-            <p className="text">해커톤 소개</p>
-            <div>
-              <textarea
-                rows={1}
-                name="description"
-                type="textarea"
-                wrap="on"
-                placeholder="자기소개"
-                value={description}
-                onChange={onDescriptionHandler}
-                className="loginregister__desc"
-              />
-            </div>
-            <div>
-              <button className="move" onClick={onClickButton}>
-                Upload
-              </button>
-            </div>
+              </div>
+              <div>
+                <button className="move" onClick={onClickButton}>
+                  Upload
+                </button>
+              </div>
+            </AlwaysScrollSection>
           </form>
         </div>
       </div>
